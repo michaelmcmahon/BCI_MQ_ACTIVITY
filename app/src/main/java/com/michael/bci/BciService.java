1b62179373f5c1952fd79b14e97f9ec1dbc426e2
Michael McMahon <mjmcmahon@outlook.ie>
2020-09-25 18:35:15 +0100

    mHandler is a Handler to deliver messages to the mSenderThread Looper's message queue and execute them on that Looper's thread. obtainMessage(), sets the what and obj members of the returned Message. what = int: Value of 10 assigned to the returned Message.what field. obj = Object: Value to dataToSend assigned to the returned Message.obj field. This value may be null I have added a comment to clarify this
    Signed-off-by: Michael McMahon <mjmcmahon@outlook.ie>

--- app/src/main/java/com/michael/bci/BciService.java
+++ app/src/main/java/com/michael/bci/BciService.java
@@ -67,108 +67,119 @@ public class BciService extends Service {
     public static final long SLEEP = 200;
     public static final int VENDOR_ID = 1027;
     public static final int BAUD_RATE = 115200;
-    public static final byte PACKET_SIZE = (byte) 33; //One byte header 0x41 then 31 bytes of data followed by 0xCX where X is 0-F in hex
-    public static final byte START_BYTE = (byte) 0xA0; //Header Byte 1: 0xA0
-    byte mStopBit = D2xxManager.FT_STOP_BITS_1;
-    byte mDataBit = D2xxManager.FT_DATA_BITS_8;
-    byte mParity = D2xxManager.FT_PARITY_NONE;
-    short mFlowControl = D2xxManager.FT_FLOW_NONE; //FT_FLOW_NONE / FT_FLOW_RTS_CTS / FT_FLOW_DTR_DSR / FT_FLOW_XON_XOFF
-    byte[] overflowBuffer = new byte[TRANSFER_SIZE*2]; //Do I need a larger buffer size
+    /* One byte header 0x41 then 31 bytes of data followed by 0xCX where X is 0-F in hex */
+    public static final byte PACKET_SIZE = (byte) 33;
+    /* Header Byte 1: 0xA0 */
+   public static final byte START_BYTE = (byte) 0xA0;
+   byte mStopBit = D2xxManager.FT_STOP_BITS_1;
+   byte mDataBit = D2xxManager.FT_DATA_BITS_8;
+   byte mParity = D2xxManager.FT_PARITY_NONE;
+   /* FT_FLOW_NONE / FT_FLOW_RTS_CTS / FT_FLOW_DTR_DSR / FT_FLOW_XON_XOFF */
+   short mFlowControl = D2xxManager.FT_FLOW_NONE;
+   byte[] overflowBuffer = new byte[TRANSFER_SIZE*2]; //Do I need a larger buffer size
 
-    private volatile boolean receiverThreadRunning;
-
-
-    //Define the Custom Action Intents for BroadcastReceiver
-    final static String DATA_RECEIVED_INTENT = "bci.intent.action.DATA_RECEIVED";
-    final static String DATA_EXTRA = "bci.intent.extra.DATA";
-    final static String SEND_COMMAND = "bci.intent.action.SEND_COMMAND";
-    final static String COMMAND_EXTRA = "bci.intent.extra.COMMAND_EXTRA";
-    final static String SEND_DATA_INTENT = "bci.intent.action.SEND_DATA";
-    final static String DATA_SENT_INTERNAL_INTENT = "bci.internal.intent.action.DATA_SENT";
+   private volatile boolean receiverThreadRunning;
 
 
-    private D2xxManager.DriverParameters mDriverParameters;
+   /* Define the Custom Action Intents for BroadcastReceiver */
+   final static String DATA_RECEIVED_INTENT = "bci.intent.action.DATA_RECEIVED";
+   final static String DATA_EXTRA = "bci.intent.extra.DATA";
+   final static String SEND_COMMAND = "bci.intent.action.SEND_COMMAND";
+   final static String COMMAND_EXTRA = "bci.intent.extra.COMMAND_EXTRA";
+   final static String SEND_DATA_INTENT = "bci.intent.action.SEND_DATA";
+   final static String DATA_SENT_INTERNAL_INTENT = "bci.internal.intent.action.DATA_SENT";
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        IntentFilter filter = new IntentFilter();
+   private D2xxManager.DriverParameters mDriverParameters;
+
+   @Override
+   public IBinder onBind(Intent intent) {
+       return null;
+   }
+
+   @Override
+   public void onCreate() {
+       super.onCreate();
+       IntentFilter filter = new IntentFilter();
 //      filter.addAction(SEND_DATA_INTENT);
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-        filter.addAction(SEND_COMMAND);
-        registerReceiver(mReceiver, filter);
-    }
+       filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+       filter.addAction(SEND_COMMAND);
+       registerReceiver(mReceiver, filter);
+   }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+   @Override
+   public int onStartCommand(Intent intent, int flags, int startId) {
 
-        //Set the Foreground notification's tap action
-        createNotificationChannel();
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+       /* Set the Foreground notification's tap action */
+       createNotificationChannel();
+       Intent notificationIntent = new Intent(this, MainActivity.class);
+       notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+       PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
-        //Set the Foreground notification content
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("BCI_1")
-                .setContentText("This ia a test foreground message for BCI Service 1")
+       /* Set the Foreground notification content */
+       Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+               .setContentTitle("BCI_1")
+               .setContentText("This ia a test foreground message for BCI Service 1")
 //                .setSmallIcon(R.drawable.icon)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .build();
+               .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+               .setContentIntent(pendingIntent)
+               .build();
 
-        //Start Foreground Service
-        startForeground(1, notification);
-
-
-        if (mIsRunning) {
-            Log.i(TAG, "Service already running.");
-            return Service.START_REDELIVER_INTENT;
-        }
-        //obtain the UsbDevice that represents the attached device from the intent-filter set in AndroidManifest.xml
-        UsbDevice mUsbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-
-        if (null == ftD2xx) {
-            try {
-                ftD2xx = D2xxManager.getInstance(this);
-                ftD2xx.createDeviceInfoList(this);
-
-            } catch (D2xxManager.D2xxException ex) {
-                Log.e(TAG, "USB: Catch exception");
-                Log.e(TAG, ex.getMessage(), ex);
-            }
-
-        }
-
-        // connect
-        if (null == ftDevice)
-        {
-            ftDevice = ftD2xx.openByUsbDevice(this, mUsbDevice);
-            Log.e(TAG, "USB: Connect to ftDevice");
-        }
+       /* Start Foreground Service */
+       startForeground(1, notification);
 
 
-        if (ftDevice == null || !ftDevice.isOpen()) {
-            Log.e(TAG, "USB: Opening ftDevice Failed");
-            stopSelf();
-            return Service.START_REDELIVER_INTENT;
-        }
+       if (mIsRunning) {
+           Log.i(TAG, "Service already running.");
+           return Service.START_REDELIVER_INTENT;
+       }
+       /* obtain the UsbDevice that represents the attached device from the intent-filter set in AndroidManifest.xml */
+       UsbDevice mUsbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
 
-        // reset to UART mode for 232 devices
-        ftDevice.setBitMode((byte) 0, D2xxManager.FT_BITMODE_RESET); // Reset FT Device
-        ftDevice.setBaudRate(BAUD_RATE); //Set BaudRat
+       if (null == ftD2xx) {
+           try {
+               ftD2xx = D2xxManager.getInstance(this);
+               ftD2xx.createDeviceInfoList(this);
 
-        ftDevice.setLatencyTimer((byte) 16); //The default FTDI 16 latency may be too large for EEG apps, making the incoming signal "choppy", May change from 16ms to 1ms
+           } catch (D2xxManager.D2xxException ex) {
+               Log.e(TAG, "USB: Catch exception");
+               Log.e(TAG, ex.getMessage(), ex);
+           }
 
-        //FTDI USB configured as serial port running at 115200 baud using typical 8-N-1.
-        byte dataBit = mDataBit; /*Individual bits representing the data character. Fewer bits reduces the range of data, but can increase the effective data transfer rate - 8:8bit, 7: 7bit etc*/
-        byte stopBit = mStopBit; /*time interval to indicate the end of that character - can be configured to remain idle for 1- or 2-bit durations.*/
-        byte parity = mParity; /*Optional error checking value to indicate if the contents of the data bits sum to an even or odd value - 0: none, 1: odd, 2: even, 3: mark, 4: space*/
+       }
+
+       /* connect */
+       if (null == ftDevice)
+       {
+           ftDevice = ftD2xx.openByUsbDevice(this, mUsbDevice);
+           Log.e(TAG, "USB: Connect to ftDevice");
+       }
+
+
+       if (ftDevice == null || !ftDevice.isOpen()) {
+           Log.e(TAG, "USB: Opening ftDevice Failed");
+           stopSelf();
+           return Service.START_REDELIVER_INTENT;
+       }
+
+       /* reset to UART mode for 232 devices */
+       ftDevice.setBitMode((byte) 0, D2xxManager.FT_BITMODE_RESET); /* Reset FT Device */
+       ftDevice.setBaudRate(BAUD_RATE); /* Set BaudRate */
+       /* The default FTDI 16 latency may be too large for EEG apps, making the incoming signal "choppy", May change from 16ms to 1ms */
+       ftDevice.setLatencyTimer((byte) 16);
+
+       /* FTDI USB configured as serial port running at 115200 baud using typical 8-N-1. */
+
+       /* Individual bits representing the data character. Fewer bits reduces the range of data,
+          but can increase the effective data transfer rate - 8:8bit, 7: 7bit etc */
+        byte dataBit = mDataBit;
+       /* time interval to indicate the end of that character - can be configured to remain
+          idle for 1- or 2-bit durations.*/
+        byte stopBit = mStopBit;
+       /*Optional error checking value to indicate if the contents of the data bits sum to an
+         even or odd value - 0: none, 1: odd, 2: even, 3: mark, 4: space*/
+        byte parity = mParity;
+        /* Pull all above together and set the Data Characteristics */
         ftDevice.setDataCharacteristics(dataBit, stopBit, parity);
 
         short flowControl = mFlowControl; /*0:none, 1: flow control(CTS,RTS)*/
@@ -181,6 +192,8 @@ public class BciService extends Service {
 
         mIsRunning = true;
         mDriverParameters = new D2xxManager.DriverParameters();
+
+        /* Get some logging output to see what is going on */
         Log.d(TAG, "USB: Buffer Number: " + mDriverParameters.getBufferNumber()); //Return Buffer number for Rx in user space application
         Log.d(TAG, "USB: Rx Buffer size: " + mDriverParameters.getMaxTransferSize()); //Return Rx buffer size of user space application
         Log.d(TAG, "USB: Max Transfer size for Rx: " + mDriverParameters.getMaxBufferSize()); //Return Max Transfer size for Rx in the user space application.
@@ -189,7 +202,6 @@ public class BciService extends Service {
         Log.d(TAG, "USB: open status of the device: " + ftDevice.isOpen()); //Returns the open status of the device
         Log.d(TAG, "USB: buffer is full -> Rx pending until read: " + ftDevice.readBufferFull()); //Returns if the Rx buffer was full with data, if true, Rx would be pending until the data is read by user.
         Log.d(TAG, "USB: LATENCY_TIMER: " + ftDevice.getLatencyTimer()); //Retrieves the current latency timer value from the device
-
         Log.i(TAG, "USB: Receiving data!");
 
         Toast.makeText(getBaseContext(), getString(R.string.receiving), Toast.LENGTH_SHORT).show();
@@ -245,7 +257,7 @@ public class BciService extends Service {
 
     }
 
-    //SEND COMMAND TO OPENBCI BOARD
+    /* SEND COMMAND TO OPENBCI BOARD */
     private final BroadcastReceiver mReceiver = new BroadcastReceiver()  {
 //        private SenderThread mSenderThread;
         @Override
@@ -261,6 +273,11 @@ public class BciService extends Service {
                         Toast.makeText(context, "No extra in Intent", Toast.LENGTH_LONG).show();
                         return;
                     }
+                     /* mHandler is a Handler to deliver messages to the mSenderThread Looper's message
+                        queue and execute them on that Looper's thread.
+                        obtainMessage(), sets the what and obj members of the returned Message.
+                        what = int: Value of 10 assigned to the returned Message.what field.
+                        obj	= Object: Value to dataToSend assigned to the returned Message.obj field. This value may be null */
                     mSenderThread.mHandler.obtainMessage(10, dataToSend).sendToTarget();
                 }
             }
