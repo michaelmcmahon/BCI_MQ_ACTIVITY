5331cc209b8c4a91712c4ab22efd89ebc6c80304
Michael McMahon <mjmcmahon@outlook.ie>
2020-10-19 11:58:53 +0100

    Added RabbitMQ Receiver Files

---
+++ RabbitMQ_Receiver_JSON/Recv.java
@@ -0,0 +1,37 @@
+//We need to import some classes
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+//Set up the class and name the queue
+public class Recv 
+{
+    private final static String QUEUE_NAME = "json-example";
+
+//then we can create a connection to the server
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("YOUR AWS EC2 IP ADDRESS"); 
+        factory.setUsername("YOUR AWS EC2 RABBITMQ USERNAME");
+        factory.setPassword("YOUR AWS EC2 RABBITMQ PASSWORD");
+        factory.setVirtualHost("/");
+        factory.setPort(5672);
+        Connection connection = factory.newConnection();
+
+//we open a channel, and declare the queue from which we're going to consume        
+	Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+//tell server to deliver messages from queue async and provide callback to buffer messages 
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
