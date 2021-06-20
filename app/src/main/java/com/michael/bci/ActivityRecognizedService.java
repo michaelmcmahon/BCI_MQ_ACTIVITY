56ec1ac577d0afea17bf64873cbb9a9eadc84603
Michael McMahon <mjmcmahon@outlook.ie>
2021-06-20 23:19:36 +0100

    Changed LinkedHashMap to Hashmap and removed if/else in favour of Switch/case for more accurate Activity Recognition
    Signed-off-by: Michael McMahon <mjmcmahon@outlook.ie>

--- app/src/main/java/com/michael/bci/ActivityRecognizedService.java
+++ app/src/main/java/com/michael/bci/ActivityRecognizedService.java
@@ -13,6 +13,7 @@ import com.rabbitmq.client.ConnectionFactory;
 import org.json.simple.JSONObject;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import org.json.simple.JSONValue;
@@ -54,7 +55,110 @@ public class ActivityRecognizedService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if(ActivityRecognitionResult.hasResult(intent)) {
             ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+            DetectedActivity mostProbableActivity = result.getMostProbableActivity();
             handleDetectedActivities( result.getProbableActivities() );
+
+            // Get the type of activity
+            int activityType = mostProbableActivity.getType();
+            // Get the confidence percentage for the most probable activity
+            int confidence = mostProbableActivity.getConfidence();
+        }
+    }
+
+    /*
+    Suppress due to an unchecked 'put(K,V)' warning because org.json.simple.JSONObject uses raw type
+    collections internally - need to change to a library which supports generics to be more type safe.
+    */
+    @SuppressWarnings(value = "unchecked")
+
+    /*
+    In the handleDetectedActivities method we connect to the RabbitMQ 'activity' Queue and send timestamped
+    data of each activity that has been detected and how confident Google Play Services is that the user is
+    performing that activity by calling getConfidence() on a DetectedActivity instance.
+     */
+    private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
+
+        try {
+
+            Calendar calendar = Calendar.getInstance(); //Get calendar using current time zone and locale of the system.
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS"); //format and parse date-time
+            long unixTime = System.currentTimeMillis() / 1000L; //Unix Timestamp
+
+            for (DetectedActivity activity : probableActivities) {
+                /* Placed RabbitMQ connection, channel, queue into their own Methods */
+                String QUEUE_NAME = "activity"; //RabbitMQ Queue Name
+                //Connection connection = getConnection();
+                Channel channel = getChannel();
+                Log.d(TAG, "RMQ: Connection/Channel 2 for EEG" +channel);
+
+                /*Encode a JSON object using Map so order of the object entries is preserved*/
+                Map obj=new HashMap();
+                //LinkedHashMap<String, java.io.Serializable> obj = new LinkedHashMap<>();
+                /* Create a new String using the date-time format */
+                String dateString = simpleDateFormat.format(calendar.getTime());
+                obj.put("TS", dateString); // Create a Timestamp
+
+                switch (activity.getType())
+                {
+                    case DetectedActivity.IN_VEHICLE: {
+                        obj.put("In Vehicle ", activity.getConfidence());
+                        Log.e("ActivityRecognition", "In Vehicle " + activity.getConfidence());
+                        break;
+                    }
+                    case DetectedActivity.ON_BICYCLE: {
+                        obj.put("On Bicycle ", activity.getConfidence());
+                        Log.e("ActivityRecognition", "On Bicycle " + activity.getConfidence());
+                        break;
+                    }
+                    case DetectedActivity.STILL: {
+                        obj.put("Still ", activity.getConfidence());
+                        Log.e("ActivityRecognition", "Still " + activity.getConfidence());
+                        break;
+                    }
+                    case DetectedActivity.TILTING: {
+                        obj.put("Tilting ", activity.getConfidence());
+                        Log.e("ActivityRecognition", "Tilting " + activity.getConfidence());
+                        break;
+                    }
+                    case DetectedActivity.WALKING: {
+                        obj.put("Walking ", activity.getConfidence());
+                        Log.e("ActivityRecognition", "Walking " + activity.getConfidence());
+                        break;
+                    }
+                    case DetectedActivity.RUNNING: {
+                        obj.put("Running ", activity.getConfidence());
+                        Log.e("ActivityRecognition", "Running " + activity.getConfidence());
+                        break;
+                    }
+                    case DetectedActivity.ON_FOOT: {
+                        obj.put("On Foot ", activity.getConfidence());
+                        Log.e("ActivityRecognition", "On Foot" + activity.getConfidence());
+                        break;
+                    }
+                    case DetectedActivity.UNKNOWN: {
+                        obj.put("Unknown ", activity.getConfidence());
+                        Log.e("ActivityRecognition", "Unknown " + activity.getConfidence());
+                        break;
+                    }
+                }
+                obj.put("UnixTS", unixTime); // Create a Unix Timestamp
+                obj.put("TS", dateString); // Create a Formatted Timestamp
+
+                channel.basicPublish("", QUEUE_NAME, null, JSONValue.toJSONString(obj).getBytes(StandardCharsets.UTF_8));
+
+                /* For now lets just close connection every time */
+                //RabbitmqConnection.CloseConnection();
+                CloseChannel();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // If we hit any issues, close the RabbitMQ channel, so that it is re-created on the next read.
+            try {
+                CloseChannel();
+                Log.d(TAG, "RMQ: Close Loop Channel for Activity");
+            } catch (IOException | TimeoutException ioException) {
+                ioException.printStackTrace();
+            }
         }
     }
 
@@ -100,101 +204,4 @@ public class ActivityRecognizedService extends IntentService {
             Log.d(TAG, "RMQ: Close Connection for Activity" + connection);
         }
     }
-
-    /*
-    Suppress due to an unchecked 'put(K,V)' warning because org.json.simple.JSONObject uses raw type
-    collections internally - need to change to a library which supports generics to be more type safe.
-    */
-   @SuppressWarnings(value = "unchecked")
-
-    /*
-    In the handleDetectedActivities method we connect to the RabbitMQ 'activity' Queue and send timestamped
-    data of each activity that has been detected and how confident Google Play Services is that the user is
-    performing that activity by calling getConfidence() on a DetectedActivity instance.
-     */
-    private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
-
-        try {
-            /* Placed RabbitMQ connection, channel, queue into their own Methods */
-            String QUEUE_NAME = "activity"; //RabbitMQ Queue Name
-
-            //Connection connection = getConnection();
-            Channel channel = getChannel();
-            Log.d(TAG, "RMQ: Connection/Channel 2 for EEG" +channel);
-
-            Calendar calendar = Calendar.getInstance(); //Get calendar using current time zone and locale of the system.
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS"); //format and parse date-time
-            for (DetectedActivity activity : probableActivities) {
-                /*Encode a JSON object using Map so order of the object entries is preserved*/
-                Map obj=new LinkedHashMap();
-                /* Create a new String using the date-time format */
-                String dateString = simpleDateFormat.format(calendar.getTime());
-                obj.put("TS", dateString); // Create a Timestamp
-
-                if (activity.getType() == DetectedActivity.IN_VEHICLE && activity.getConfidence() >= 1) {
-                    obj.put("In Vehicle: ", +activity.getConfidence());
-                    Log.e("ActivityRecognition", "In Vehicle: " + activity.getConfidence());
-                } else {
-                    obj.put("In Vehicle:", 0);
-                }
-                if (activity.getType() == DetectedActivity.ON_BICYCLE && activity.getConfidence() >= 1) {
-                    obj.put("On Bicycle: ", +activity.getConfidence());
-                    Log.e("ActivityRecognition", "On Bicycle: " + activity.getConfidence());
-                } else {
-                    obj.put("On Bicycle:", 0);
-                }
-                if (activity.getType() == DetectedActivity.ON_FOOT && activity.getConfidence() >= 1) {
-                    obj.put("On Foot: ", +activity.getConfidence());
-                    Log.e("ActivityRecognition", "On Foot: " + activity.getConfidence());
-                } else {
-                    obj.put("On Foot:", 0);
-                }
-                if (activity.getType() == DetectedActivity.RUNNING && activity.getConfidence() >= 1) {
-                    obj.put("Running: ", +activity.getConfidence());
-                    Log.e("ActivityRecognition", "Running: " + activity.getConfidence());
-                } else {
-                    obj.put("Running:", 0);
-                }
-                if (activity.getType() == DetectedActivity.STILL && activity.getConfidence() >= 1) {
-                    obj.put("Still: ", +activity.getConfidence());
-                    Log.e("ActivityRecognition", "Still: " + activity.getConfidence());
-                } else {
-                    obj.put("Still:", 0);
-                }
-                if (activity.getType() == DetectedActivity.TILTING && activity.getConfidence() >= 1) {
-                    obj.put("Tilting: ", +activity.getConfidence());
-                    Log.e("ActivityRecognition", "Tilting: " + activity.getConfidence());
-                } else {
-                    obj.put("Tilting:", 0);
-                }
-                if (activity.getType() == DetectedActivity.WALKING && activity.getConfidence() >= 1) {
-                    obj.put("Walking: ", +activity.getConfidence());
-                    Log.e("ActivityRecognition", "Walking: " + activity.getConfidence());
-                } else {
-                    obj.put("Walking:", 0);
-                }
-                if (activity.getType() == DetectedActivity.UNKNOWN && activity.getConfidence() >= 1) {
-                    obj.put("Unknown: ", +activity.getConfidence());
-                    Log.e("ActivityRecognition", "Unknown: " + activity.getConfidence());
-                } else {
-                    obj.put("Unknown:", 0);
-                }
-                channel.basicPublish("", QUEUE_NAME, null, JSONValue.toJSONString(obj).getBytes(StandardCharsets.UTF_8));
-
-                /* For now lets just close connection every time */
-                //RabbitmqConnection.CloseConnection();
-                CloseChannel();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            // If we hit any issues, close the RabbitMQ channel, so that it is re-created on the next read.
-            try {
-                CloseChannel();
-                Log.d(TAG, "RMQ: Close Loop Channel for Activity");
-            } catch (IOException | TimeoutException ioException) {
-                ioException.printStackTrace();
-            }
-        }
-    }
 }
-
